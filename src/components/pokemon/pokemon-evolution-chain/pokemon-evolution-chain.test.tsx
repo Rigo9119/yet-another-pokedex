79c6809c35169da5692e8d2345be363eaf4c6f4e
Rigo Rosero Castillo <rigo4791@gmail.com>
2026-05-10 09:56:09 -0500

    adds pokemon-evolutions-chain

--- src/components/pokemon/pokemon-evolution-chain/pokemon-evolution-chain.test.tsx
+++ src/components/pokemon/pokemon-evolution-chain/pokemon-evolution-chain.test.tsx
@@ -1,9 +1,135 @@
 import { describe, expect, vi, test, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import PokemonEvolutionChain from "./pokemon-evolution-chain";
+import { PokemonLocalized } from "@/data/types";
 
 vi.mock("@/paraglide/messages", () => ({
   m: {
     evolutions_chain_title: () => "Evolutions",
   },
 }));
+
+vi.mock("@/components/pokemon-image-card", () => ({
+  default: ({ pokemon }: { pokemon: PokemonLocalized }) => (
+    <div data-testid="pokemon-card">{pokemon.name}</div>
+  ),
+}));
+
+describe("PokemonEvolutionChain", () => {
+  const mockEvolutions: PokemonLocalized[] = [
+    {
+      id: 1,
+      name: "bulbasaur",
+      sprites: {
+        back_default: "",
+        back_female: "",
+        back_shiny: "",
+        back_shiny_female: "",
+        front_default: "",
+        front_female: "",
+        front_shiny: "",
+        front_shiny_female: "",
+      },
+      types: [],
+      stats: [],
+      abilities: [],
+      moves: [],
+      height: 12,
+      weight: 12,
+      order: 1,
+      base_experience: 123,
+      is_default: false,
+      species: { url: "", name: "bulbasaur" },
+      cries: { latest: "", legacy: "" },
+      forms: [],
+      game_indices: { game_index: 12, version: { url: "", name: "" } },
+      held_items: [],
+      location_area_encounters: "",
+      past_abilities: [],
+      past_types: [],
+      localized_name: "bulbasaur",
+      flavor_text: "",
+      evolution_chain_url: "",
+    },
+    {
+      id: 2,
+      name: "ivysaur",
+      sprites: {
+        back_default: "",
+        back_female: "",
+        back_shiny: "",
+        back_shiny_female: "",
+        front_default: "",
+        front_female: "",
+        front_shiny: "",
+        front_shiny_female: "",
+      },
+      types: [],
+      stats: [],
+      abilities: [],
+      moves: [],
+      height: 12,
+      weight: 12,
+      order: 2,
+      base_experience: 123,
+      is_default: false,
+      species: { url: "", name: "ivysaur" },
+      cries: { latest: "", legacy: "" },
+      forms: [],
+      game_indices: { game_index: 12, version: { url: "", name: "" } },
+      held_items: [],
+      location_area_encounters: "",
+      past_abilities: [],
+      past_types: [],
+      localized_name: "ivysaur",
+      flavor_text: "",
+      evolution_chain_url: "",
+    },
+    {
+      id: 3,
+      name: "venusaur",
+      sprites: {
+        back_default: "",
+        back_female: "",
+        back_shiny: "",
+        back_shiny_female: "",
+        front_default: "",
+        front_female: "",
+        front_shiny: "",
+        front_shiny_female: "",
+      },
+      types: [],
+      stats: [],
+      abilities: [],
+      moves: [],
+      height: 12,
+      weight: 12,
+      order: 3,
+      base_experience: 123,
+      is_default: false,
+      species: { url: "", name: "venusaur" },
+      cries: { latest: "", legacy: "" },
+      forms: [],
+      game_indices: { game_index: 12, version: { url: "", name: "" } },
+      held_items: [],
+      location_area_encounters: "",
+      past_abilities: [],
+      past_types: [],
+      localized_name: "venusaur",
+      flavor_text: "",
+      evolution_chain_url: "",
+    },
+  ];
+
+  beforeEach(() => {
+    render(<PokemonEvolutionChain evolutions={mockEvolutions} />);
+  });
+
+  test("renders component title", () => {
+    expect(screen.getByText("Evolutions")).toBeInTheDocument();
+  });
+
+  test("pokemons cards should be of length 3", () => {
+    expect(screen.getAllByTestId("pokemon-card")).toHaveLength(3);
+  });
+});
--- src/components/pokemon/pokemon-stats/pokemon-stats.test.tsx
+++ src/components/pokemon/pokemon-stats/pokemon-stats.test.tsx
@@ -1,6 +1,7 @@
 import { describe, expect, vi, test, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import PokemonStats from "./pokemon-stats";
+import { Stat } from "@/data/types";
 
 vi.mock("@/paraglide/messages", () => ({
   m: {
@@ -9,7 +10,7 @@ vi.mock("@/paraglide/messages", () => ({
 }));
 
 describe("PokemonStats", () => {
-  const mockStats = [
+  const mockStats: Stat[] = [
     { base_stat: 10, effort: 10, stat: { name: "stat1", url: "" } },
     { base_stat: 20, effort: 20, stat: { name: "stat2", url: "" } },
     { base_stat: 30, effort: 30, stat: { name: "stat3", url: "" } },
@@ -20,12 +21,12 @@ describe("PokemonStats", () => {
   });
 
   test("renders stats title", () => {
-    expect(screen.getByText("Stats")).toHaveTextContent("Stats");
+    expect(screen.getByText("Stats")).toBeInTheDocument();
   });
 
   test("renders stats", () => {
-    expect(screen.getByText(/stat1/)).toHaveTextContent("stat1");
-    expect(screen.getByText(/stat2/)).toHaveTextContent("stat2");
-    expect(screen.getByText(/stat3/)).toHaveTextContent("stat3");
+    expect(screen.getByText(/stat1/)).toBeInTheDocument();
+    expect(screen.getByText(/stat2/)).toBeInTheDocument();
+    expect(screen.getByText(/stat3/)).toBeInTheDocument();
   });
 });
